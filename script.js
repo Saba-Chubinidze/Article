93d88e0ac3e832898bd88d7b37f4b39db13ec0e2
Saba-Chubinidze <jeff45901@gmail.com>
2022-11-12 14:33:02 +0400

    done

---
+++ script.js
@@ -0,0 +1,6 @@
+let burger = document.querySelector(".menu_icon");
+let mobileMenu = document.querySelector(".ul_one");
+
+burger.addEventListener("click", function(){
+    mobileMenu.classList.toggle("active");
+})
